3f5de1793b1219ea4c4da102200b495218bb23a7
Herman Mossur <pro-german@ukr.net>
2023-08-06 08:44:19 +0300

    config for marathon page

---
+++ src/marathon.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import marathon from './marathon.vue'
+
+createApp(marathon).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    },
+    marathon: {
+      entry: 'src/marathon.js',
+      template: 'public/index.html',
+      filename: 'marathon.html',
+      title: 'Marathon'
+    }
+  },
   transpileDependencies: true,
   publicPath: process.env.VUE_APP_TYPE === 'gh' 
     ? '/holota-landing/'
